cb4e54ed938a19912ee5c2aa1aebdda912f81808
Udi Oron <udioron@gmail.com>
2018-12-27 11:46:04 +0200

    added an empty expenses app

--- budget_police/settings.py
+++ budget_police/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'expenses',
+    # 'expenses.apps.ExpensesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
