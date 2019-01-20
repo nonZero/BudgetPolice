b38ba835099beeca0c8aa0f135e4c81b32e10463
Udi Oron <udioron@gmail.com>
2019-01-20 15:08:21 +0200

    simple form

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.shortcuts import render, get_object_or_404
 
 from expenses.models import Expense
@@ -17,8 +18,25 @@ def expense_detail(request, pk):
     })
 
 
+class ExpenseForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    amount = forms.DecimalField(decimal_places=2, max_digits=10, required=False)
+    are_you_sure = forms.BooleanField()
+    flavours = forms.MultipleChoiceField(choices=(
+        (1, 'Vanilla'),
+        (2, 'Strwberry'),
+        (3, 'Chochlate'),
+    ))
+
+
 def expense_create(request):
     if request.method == "POST":
-        assert False, request.POST
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            assert False, form.cleaned_data
+    else:
+        form = ExpenseForm()
+
     return render(request, "expenses/expense_form.html", {
+        'form': form,
     })
